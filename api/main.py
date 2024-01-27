53379c7cdd75c121c933c364e04db45e9c682bc6
Freddie Briden <freddie@Freddies-MacBook-Air.local>
2024-01-27 14:55:26 -0500

    Added some mongodb interaction in the backend

--- api/main.py
+++ api/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated, Optional
+from pydantic.functional_validators import BeforeValidator
+from pydantic import BaseModel, Field
+
+import motor.motor_asyncio as motor
+
+import os
 
 app = FastAPI()
 
@@ -13,6 +20,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class BusinessModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    name: str = Field(...)
+
+client = motor.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.get_database("users")
+business_collection = db.get_collection("businesses")
+
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
+
+@app.post("/addbusiness/")
+async def add_business(business: BusinessModel):
+    new_business = await business_collection.insert_one(
+        business.model_dump(by_alias=True, exclude=["id"])
+    )
+    created_business = await business_collection.find_one(
+        {"_id": new_business.inserted_id}
+    )
+    return created_business

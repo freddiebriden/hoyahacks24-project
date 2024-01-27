7951e4c5e58feac57830f6b0f87296ced66349f0
Freddie Briden <freddie@Freddies-MacBook-Air.local>
2024-01-27 16:05:21 -0500

    Connects to mongodb now kinda

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Annotated, Optional
+from typing import Annotated, Optional, List
 from pydantic.functional_validators import BeforeValidator
 from pydantic import BaseModel, Field
 
@@ -19,13 +19,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+'''
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 class BusinessModel(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
     name: str = Field(...)
-
+'''
 client = motor.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 db = client.get_database("users")
 business_collection = db.get_collection("businesses")
@@ -33,13 +33,22 @@ business_collection = db.get_collection("businesses")
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+    
+@app.get("/addbusiness/{business}")
+async def add_business(business: str):
+    print(business)
+    new_business = await business_collection.insert_one({"name": business})
+    print("created")
+    return "help"
+#    created_business = await business_collection.find_one(
+#        {"_id": new_business.inserted_id}
+#    )
+#    return created_business
 
-@app.post("/addbusiness/")
-async def add_business(business: BusinessModel):
-    new_business = await business_collection.insert_one(
-        business.model_dump(by_alias=True, exclude=["id"])
-    )
-    created_business = await business_collection.find_one(
-        {"_id": new_business.inserted_id}
-    )
-    return created_business
+@app.get("/business/{name}")
+async def get_business(name: str):
+    if (
+        business := await business_collection.find_one({"name": name})
+    ) is not None:
+        return business
+    return "uh oh"

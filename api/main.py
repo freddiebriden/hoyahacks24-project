8d605c83489528e77b5e834a14e7472dcd64de5a
Freddie Briden <freddie@Freddies-MacBook-Air.local>
2024-01-27 18:35:46 -0500

    Finished add_investor and add_business

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated, Optional, List
 from pydantic.functional_validators import BeforeValidator
 from pydantic import BaseModel, Field
+from enum import Enum
 
 import motor.motor_asyncio as motor
 
@@ -19,36 +20,84 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-'''
+
+class Industry(str, Enum):
+    food = "food"
+    retail = "retail"
+    service = "service"
+    hospitality = "hospitality"
+    tech = "tech"
+    other = "other"
+
+class Funding(str, Enum):
+    seed = "seed"
+    scale = "scale"
+    expand = "expand"
+
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 class BusinessModel(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
     name: str = Field(...)
-'''
-client = motor.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+    industry: List[Industry] = Field(...)
+    subindustry: str = Field(...)
+    funding: Funding = Field(...)
+    description: str = Field(...)
+    firstName: str = Field(...)
+    lastName: str = Field(...)
+    email: str = Field(...)
+    liked: List[PyObjectId] = []
+
+class InvestorModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    name: str = Field(...)
+    industry: List[Industry] = Field(...)
+    funding: Funding = Field(...)
+    description: str = Field(...)
+    firstName: str = Field(...)
+    lastName: str = Field(...)
+    email: str = Field(...)
+    seen: List[PyObjectId] = []
+'{"name": "temp", "industry": "food", "subindustry": "sushi", "funding": "seed", "description": "good food", "firstName": "me", "lastName": "me:3", "email": "email address"}'
+client = motor.AsyncIOMotorClient('mongodb+srv://freddiebriden:rPf3IfAprDkH1h3m@cluster0.jw0jeya.mongodb.net/?retryWrites=true&w=majority')
 db = client.get_database("users")
 business_collection = db.get_collection("businesses")
+investor_collection = db.get_collection("investors")
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-    
-@app.get("/addbusiness/{business}")
-async def add_business(business: str):
-    print(business)
-    new_business = await business_collection.insert_one({"name": business})
-    print("created")
-    return "help"
-#    created_business = await business_collection.find_one(
-#        {"_id": new_business.inserted_id}
-#    )
-#    return created_business
+
+@app.post("/addbusiness/")
+async def add_business(business: BusinessModel):
+    print(business.name)
+    new_business = await business_collection.insert_one(
+        business.model_dump(by_alias=True, exclude=["id"])
+    )
+    print(new_business.inserted_id)
+    created_business = await business_collection.find_one(
+        {"_id": new_business.inserted_id}
+    )
+    print(created_business)
+    return "bleh"
+
+@app.post("/addinvestor/")
+async def add_investor(investor: InvestorModel):
+    print(investor.name)
+    new_investor = await investor_collection.insert_one(
+        investor.model_dump(by_alias=True, exclude=["id"])
+    )
+    print(new_investor.inserted_id)
+    created_investor = await investor_collection.find_one(
+        {"_id": new_investor.inserted_id}
+    )
+    print(created_investor)
+    return "bleh"
 
 @app.get("/business/{name}")
 async def get_business(name: str):
     if (
         business := await business_collection.find_one({"name": name})
     ) is not None:
-        return business
-    return "uh oh"
+        print(business)
+    return "yeah"
